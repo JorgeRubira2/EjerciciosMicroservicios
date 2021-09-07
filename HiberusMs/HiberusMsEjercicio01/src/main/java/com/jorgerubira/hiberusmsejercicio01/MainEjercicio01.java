68b4cccfcaf4eb09ea9c362900e8dd8747505403
JorgeRubira <jorgesub@gmail.com>
2021-09-07 09:20:31 +0200

    Merge origin/master into HEAD
    Conflicts:
    	.gitignore
    	src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio00PruebaDeTestBasica.java

---
+++ HiberusMs/HiberusMsEjercicio01/src/main/java/com/jorgerubira/hiberusmsejercicio01/MainEjercicio01.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jorgerubira.hiberusmsejercicio01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainEjercicio01 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainEjercicio01.class, args);
+    }
+}
