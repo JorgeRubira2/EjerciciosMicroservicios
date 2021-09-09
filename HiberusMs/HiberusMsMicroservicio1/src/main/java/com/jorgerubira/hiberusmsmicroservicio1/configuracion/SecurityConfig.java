addd8da081fd39e5ac1a7d479c004d73b3047330
JorgeRubira <jorgesub@gmail.com>
2021-09-09 11:42:30 +0200

    Merge origin/master into HEAD
    Conflicts:
    	.gitignore
    	src/main/java/com/jorgerubira/ejerciciosjava/Ejercicio00PruebaDeTestBasica.java

---
+++ HiberusMs/HiberusMsMicroservicio1/src/main/java/com/jorgerubira/hiberusmsmicroservicio1/configuracion/SecurityConfig.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jorgerubira.hiberusmsmicroservicio1.configuracion;
+
+import com.jorgerubira.hiberusmsmicroservicio1.services.ValidacionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@Configuration
+//@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private ValidacionService myUserDetailService;    
+    
+    //Aquí se configura el acceso
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+             .antMatchers("/url1").permitAll()
+             .antMatchers("/login").permitAll()
+             .antMatchers("/altaUsuario").permitAll()
+             .antMatchers("/url2").hasAnyRole("Administrador", "Usuario")
+             .antMatchers("/vuelos/**").hasAnyRole("Administrador")
+             .antMatchers("/traducir/**").hasAnyRole("Administrador", "Usuario")
+             .antMatchers("/url3").hasRole("Administrador");
+             
+        http.formLogin();   //  /login. Spring
+        http.csrf().disable();
+        //http.formLogin();   //  /login. Spring
+    }
+
+    
+    //Aquí se configura Usuario/Password
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /*auth.inMemoryAuthentication()
+                 .withUser("jorge").password("{noop}1111").roles("Usuario")
+                 .and()
+                 .withUser("juan").password("{noop}1111").roles("Administrador");*/
+        auth.userDetailsService(myUserDetailService);
+    }
+    
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
+    
+    
+}
---
+++ HiberusMs/HiberusMsMicroservicio1/src/main/java/com/jorgerubira/hiberusmsmicroservicio1/dto/UsuarioDto.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jorgerubira.hiberusmsmicroservicio1.dto;
+
+import java.util.Collection;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDto implements UserDetails {
+
+    private String nombre;
+    private String password;
+    private List<RolDto> roles;
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return nombre;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+}
